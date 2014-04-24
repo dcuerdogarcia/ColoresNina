7b942e01bf21c6b9fec44ae7c875c6232a722165
jarroba <jarrobaweb@gmail.com>
2014-04-24 20:59:27 +0200

    modificación en la clase main

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		SeleccionFutbol delBosque = new Entrenador(1, "Vicente", "Del Bosque", 60, 28489);
-		SeleccionFutbol iniesta = new Futbolista();
+		SeleccionFutbol iniesta = new Futbolista(2, "Andres", "Iniesta", 29, 6, "Interior Derecho");
 		SeleccionFutbol raulMartinez = new Masajista(3, "Raúl", "Martinez", 41, "Licenciado en Fisioterapia", 18);
 
 		integrantes.add(delBosque);
